9a3a720f357b4652ec9c6901176018a01349d966
lelezsrmn <55153450+lelezsrmn@users.noreply.github.com>
2024-03-30 17:24:50 +0100

    del bcrypt

--- bdd/api.js
+++ bdd/api.js
@@ -39,6 +39,6 @@ app.get('/danger', async (req, res) => {
     }*/
 })
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(3030, () => {
+    console.log('Server listening on port 3030');
 });
