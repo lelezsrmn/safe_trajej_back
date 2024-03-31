e8b4997de7f81521d0940341729dba4a29848f64
MZiserman <martinziserman@gmail.com>
2024-03-31 14:20:14 +0200

    handle cors

--- bdd/api.js
+++ bdd/api.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const Bdd_client = require("./Bdd_client");
 const Bdd_danger = require("./Bdd_danger");
+const cors = require('cors')
+
 const app = express();
+var allowedOrigins = [
+    'http://localhost:3000',
+    'http://safetrajej.com'
+];
+
+app.use(cors(({
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+})))
 
 app.get('/client', async (req, res) => {
     let bdd = new Bdd_client();
@@ -27,10 +47,10 @@ app.get('/danger', async (req, res) => {
         let bdd = new Bdd_danger();
         await bdd.connect();
 
-        //
         let positionStr = req.query.position;
         let position = positionStr.split(',').map(Number);
         let distance = parseFloat(req.query.distance);
+
         let ping_danger = await bdd.get_danger_from_position(position, distance);
         res.send(JSON.stringify(ping_danger));
      } catch (error) {
